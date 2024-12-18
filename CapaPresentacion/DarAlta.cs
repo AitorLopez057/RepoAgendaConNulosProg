c688febc93e3f6ca94b3560037fb6b53a4b92ffe
Asier <asielng2a@gmail.com>
2024-12-18 13:15:16 +0100

    Variable gestion y telefonos en el form DarAlta

--- CapaPresentacion/DarAlta.cs
+++ CapaPresentacion/DarAlta.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidades;
+using GestionAgenda;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,24 @@ namespace CapaPresentacion
 {
     public partial class DarAlta : Form
     {
+        List<Telefonos> telefonos;
+        Gestion gestion;
         public DarAlta()
         {
             InitializeComponent();
+            telefonos = new List<Telefonos>();
+            gestion = new Gestion();
+            
+        }
+
+        private void btnAñadirContacto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAñadirTelefono_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
