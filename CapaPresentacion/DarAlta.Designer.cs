2db1d7cbe61da4272cb398e08101a2c7ff5f2a44
Danel Rico <danelricofer@gmail.com>
2024-12-18 13:01:14 +0100

    Formularios creados

--- CapaPresentacion/DarAlta.Designer.cs
+++ CapaPresentacion/DarAlta.Designer.cs
@@ -28,27 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtEliminarTelefono = new System.Windows.Forms.TextBox();
             this.txtEliminarTelefonoContacto = new System.Windows.Forms.TextBox();
             this.txtEliminarContacto = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnBorrarTelefono = new System.Windows.Forms.Button();
-            this.btnBorrarContacto = new System.Windows.Forms.Button();
+            this.btnAñadirGrupo = new System.Windows.Forms.Button();
+            this.btnAñadirContacto = new System.Windows.Forms.Button();
             this.lblResultado = new System.Windows.Forms.Label();
             this.lblBTelefono = new System.Windows.Forms.Label();
             this.lblBContacto = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // txtEliminarTelefono
-            // 
-            this.txtEliminarTelefono.Location = new System.Drawing.Point(751, 229);
-            this.txtEliminarTelefono.Name = "txtEliminarTelefono";
-            this.txtEliminarTelefono.Size = new System.Drawing.Size(231, 22);
-            this.txtEliminarTelefono.TabIndex = 21;
             // 
             // txtEliminarTelefonoContacto
             // 
@@ -64,23 +61,14 @@
             this.txtEliminarContacto.Size = new System.Drawing.Size(177, 22);
             this.txtEliminarContacto.TabIndex = 19;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(655, 229);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 16);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "TELÉFONO";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(655, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.Size = new System.Drawing.Size(118, 16);
             this.label2.TabIndex = 17;
-            this.label2.Text = "NOMBRE CONTACTO";
+            this.label2.Text = "NOMBRE GRUPO";
             // 
             // label1
             // 
@@ -91,30 +79,30 @@
             this.label1.TabIndex = 16;
             this.label1.Text = "NOMBRE CONTACTO";
             // 
-            // btnBorrarTelefono
+            // btnAñadirGrupo
             // 
-            this.btnBorrarTelefono.Location = new System.Drawing.Point(678, 351);
-            this.btnBorrarTelefono.Name = "btnBorrarTelefono";
-            this.btnBorrarTelefono.Size = new System.Drawing.Size(271, 65);
-            this.btnBorrarTelefono.TabIndex = 15;
-            this.btnBorrarTelefono.Text = "Borrar Telefono";
-            this.btnBorrarTelefono.UseVisualStyleBackColor = true;
+            this.btnAñadirGrupo.Location = new System.Drawing.Point(698, 227);
+            this.btnAñadirGrupo.Name = "btnAñadirGrupo";
+            this.btnAñadirGrupo.Size = new System.Drawing.Size(271, 65);
+            this.btnAñadirGrupo.TabIndex = 15;
+            this.btnAñadirGrupo.Text = "DAR ALTA UN GRUPO";
+            this.btnAñadirGrupo.UseVisualStyleBackColor = true;
             // 
-            // btnBorrarContacto
+            // btnAñadirContacto
             // 
-            this.btnBorrarContacto.Location = new System.Drawing.Point(89, 351);
-            this.btnBorrarContacto.Name = "btnBorrarContacto";
-            this.btnBorrarContacto.Size = new System.Drawing.Size(271, 65);
-            this.btnBorrarContacto.TabIndex = 14;
-            this.btnBorrarContacto.Text = "Borrar Contacto";
-            this.btnBorrarContacto.UseVisualStyleBackColor = true;
+            this.btnAñadirContacto.Location = new System.Drawing.Point(135, 438);
+            this.btnAñadirContacto.Name = "btnAñadirContacto";
+            this.btnAñadirContacto.Size = new System.Drawing.Size(271, 65);
+            this.btnAñadirContacto.TabIndex = 14;
+            this.btnAñadirContacto.Text = "AÑADIR CONTACTO";
+            this.btnAñadirContacto.UseVisualStyleBackColor = true;
             // 
             // lblResultado
             // 
             this.lblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.lblResultado.Location = new System.Drawing.Point(66, 33);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(731, 46);
+            this.lblResultado.Size = new System.Drawing.Size(922, 46);
             this.lblResultado.TabIndex = 13;
             // 
             // lblBTelefono
@@ -122,9 +110,9 @@
             this.lblBTelefono.AutoSize = true;
             this.lblBTelefono.Location = new System.Drawing.Point(756, 120);
             this.lblBTelefono.Name = "lblBTelefono";
-            this.lblBTelefono.Size = new System.Drawing.Size(118, 16);
+            this.lblBTelefono.Size = new System.Drawing.Size(131, 16);
             this.lblBTelefono.TabIndex = 12;
-            this.lblBTelefono.Text = "Borrar un Telefono";
+            this.lblBTelefono.Text = "Dar de Alta un Grupo";
             // 
             // lblBContacto
             // 
@@ -137,7 +125,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(229, 204);
+            this.textBox1.Location = new System.Drawing.Point(229, 191);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(177, 22);
             this.textBox1.TabIndex = 23;
@@ -145,27 +133,81 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 210);
+            this.label4.Location = new System.Drawing.Point(66, 194);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 16);
+            this.label4.Size = new System.Drawing.Size(72, 16);
             this.label4.TabIndex = 22;
-            this.label4.Text = "NOMBRE CONTACTO";
+            this.label4.Text = "ID GRUPO";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(378, 22);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "DESCRIPCIÓN";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 251);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(218, 22);
+            this.textBox3.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "TELEFONO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(148, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Telefonos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 56);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "AÑADIR TELEFONO";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // DarAlta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1127, 615);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtEliminarTelefono);
             this.Controls.Add(this.txtEliminarTelefonoContacto);
             this.Controls.Add(this.txtEliminarContacto);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnBorrarTelefono);
-            this.Controls.Add(this.btnBorrarContacto);
+            this.Controls.Add(this.btnAñadirGrupo);
+            this.Controls.Add(this.btnAñadirContacto);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.lblBTelefono);
             this.Controls.Add(this.lblBContacto);
@@ -177,19 +219,22 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtEliminarTelefono;
         private System.Windows.Forms.TextBox txtEliminarTelefonoContacto;
         private System.Windows.Forms.TextBox txtEliminarContacto;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnBorrarTelefono;
-        private System.Windows.Forms.Button btnBorrarContacto;
+        private System.Windows.Forms.Button btnAñadirGrupo;
+        private System.Windows.Forms.Button btnAñadirContacto;
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Label lblBTelefono;
         private System.Windows.Forms.Label lblBContacto;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
